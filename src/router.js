3d052a31a01caff6ac27712c30593787e562f3e4
afifisuha <afifisuha@campus.technion.ac.il>
2018-07-31 16:33:15 +0300

    #4 added if statements to router

--- src/router.js
+++ src/router.js
@@ -1,9 +1,18 @@
-const handlers = require("./handlers");
+const publichand = require("./handlers/publicHandlers");
+const serverhand = require("./handlers/serverHandlers");
 
-const router=(req,res) =>{
+const router = (req, res) => {
+  let url = req.url;
+  if (
+    url === "/" ||
+    ["/index.js", "/style.css", "/index.html", "/xhr.js"].includes(url)
+  ) {
+    publichand.handlerPublic(req, res);
+  } else if (url.includes("apinews")) {
+    serverhand.handlerAPI(req, res);
+  } else {
+    publichand.handler404(res);
+  }
+};
 
-}
-
-
-
-module.exports=router;
+module.exports = router;
