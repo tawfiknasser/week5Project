c2232ea5d291015c7d095073433fe9b06c6fdeb5
afifisuha <afifisuha@campus.technion.ac.il>
2018-07-31 17:11:23 +0300

    #4 added 500&404handelfunction

--- src/handlers/publicHandlers.js
+++ src/handlers/publicHandlers.js
@@ -1,25 +1,34 @@
-const path =require("path")
-const fs=require('fs')
-const searchCountry=require('../searchCountry')
+const path = require("path");
+const fs = require("fs");
+const searchCountry = require("../searchCountry");
 
-const handler500=(res)=>{
+const handler500 = res => {
+  res.writeHead(500, { "content-type": "text/plain" });
+  res.end("server error");
+};
+const handler404 = res => {
+  fs.readFile(
+    path.join(__dirname, "..", "..", "public", "404.html"),
 
-}
-const handler404=(res){
+    (err, file) => {
+      if (err) {
+        handler500(res);
+        return;
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(file);
+      }
+    }
+  );
+};
 
-}
+const handlerPublic = (req, res) => {};
 
-const handlerPublic=(req,res)=>{
+const handlerCountries = (req, res) => {};
 
-}
-
-const handlerCountries=(req,res)=>{
-
-}
-
-module.exports={
+module.exports = {
   handler500,
   handler404,
   handlerPublic,
   handlerCountries
-}
+};
