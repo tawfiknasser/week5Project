e56c9f2ec2ef9c602a7c33b5c09bd04ff45e6801
tawfiknasser <tawfiknasser@gmail.com>
2018-07-31 16:09:24 +0300

    #4 adde publichandlers  skeleton

---
+++ src/handlers/publicHandlers.js
@@ -0,0 +1,18 @@
+const path =require("path")
+const fs=require('fs')
+const searchCountry=require('../searchCountry')
+
+const handler500=(res)=>{
+
+}
+const handler404=(res){
+
+}
+
+const handlerPublic=(req,res)=>{
+
+}
+
+const handlerCountries=(req,res)=>{
+  
+}
