0a2a967b26f65eb1dc7b7ecfe2abf3456fd2c506
tawfiknasser <tawfiknasser@gmail.com>
2018-08-01 16:38:30 +0300

    #14 changed XhrApi to APIWrapper+ added comments

--- src/handlers/serverHandlers.js
+++ src/handlers/serverHandlers.js
@@ -10,8 +10,8 @@ const handlerAPI = (req, res) => {
   // should handle the url with catagory
 
   let API_KEY = "01b3313330fc4ce993dbddc83190e60f";
-  let countryName = "United Kingdom"; // should delete __
-  let SelectedCategory = "From the Link";
+  let countryName = "United Kingdom"; // should delete __   // build more function
+  let SelectedCategory = "From the Link"; // build more function
   let coutryShortcut = searchCountry(countryName);
   // res.writeHead(200,{"Content-Type"})
   // res.end(JSON.stringify({countryShortcut}));
@@ -35,15 +35,16 @@ const handlerAPI = (req, res) => {
  //=========== Send the request down here =======================
 
 
-  xhrAPI(url, xhrRes => {  // should be APIWrapper ?
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(xhrRes);
+APIWrapper(url, response => {
+   res.writeHead(200, { "Content-Type": "application/json" });
+   res.end(response);
   });
 };  // ======= the end of handlerAPI function
 
 const APIWrapper = (url, cb) => {
   request(url, function(error, response, body) {
     if (!error) cb(body);
+
   });
 };
 
