6d2f94341272cd23b30fdc0d46a1364b8a37c4b1
Noor Afifi <noor261998afifi@gmail.com>
2018-08-01 17:08:20 +0300

    deleted image folder

--- src/router.js
+++ src/router.js
@@ -6,25 +6,17 @@ const router = (req, res) => {
   if (url === "/Test500") publichand.handlerTest500(req, res);
   else if (
     url === "/" ||
-    [
-      "/index.js",
-      "/style.css",
-      "/images/pro-news-bg.jpeg",
-      "/index.html",
-      "/xhr.js"
-    ].includes(url)
+    ["/index.js", "/style.css", "/index.html", "/xhr.js"].includes(url)
   ) {
     publichand.handlerPublic(req, res);
-  } else if (url.indexOf("selected")==0) {
+  } else if (url.indexOf("selected") == 0) {
     serverhand.handlerAPI(req, res);
   }
 
   // else if(url.includes("searchMeCountry")) {
   //   publichand.handlerCountries(req,res);
   // }
-
-
-   else {
+  else {
     publichand.handler404(res);
   }
 };
