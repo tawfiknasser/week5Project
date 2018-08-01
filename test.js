3b9af03b776106015ddb9bfdafb6473a52d3c11f
afifisuha <afifisuha@campus.technion.ac.il>
2018-08-01 10:19:13 +0300

    #4 added tests

---
+++ test.js
@@ -0,0 +1,31 @@
+const test = require("tape");
+const router = require("./src/router");
+const supertest = require("supertest");
+
+test("Initialize", t => {
+  let num = 2;
+  t.equal(num, 2, "should return 2");
+  t.end();
+});
+test("should return 200", t => {
+  supertest(router)
+    .get("/")
+    .expect(200)
+    .expect("Content-Type", /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.statusCode, 200, "should return 200");
+      t.end();
+    });
+});
+test("should return 404", t => {
+  supertest(router)
+    .get("/jfiejow")
+    .expect(500)
+    .expect("Content-Type", /plain/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.statusCode, 404, "should respond with page not found");
+      t.end();
+    });
+});
